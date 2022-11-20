b6b1f744d0bf99d0c31f650ff13ac7188ffcaae2
chenghao xie <1152759491@qq.com>
2022-11-20 12:13:04 +0000

    peoplepage

---
+++ movies/cypress/e2e/peoplebase.cy.js
@@ -0,0 +1,58 @@
+let persons; // List of movies from TMDB
+let people; //
+
+describe("Base tests", () => {
+  before(() => {
+    // Get the discover movies from TMDB and store them locally.
+    cy.request(
+      `https://api.themoviedb.org/3/person/popular?api_key=${Cypress.env(
+        "TMDB_KEY"
+      )}&language=en-US&page=1`
+    )
+      .its("body") // Take the body of HTTP response from TMDB
+      .then((response) => {
+        persons = response.results;
+      });
+  });
+  beforeEach(() => {
+    cy.visit("/movies/people");
+  });
+
+  describe("The People page", () => {
+    it("displays the page header and 20 people", () => {
+        cy.get("h3").contains("Famous People");
+      cy.get(".MuiCardHeader-root").should("have.length", 20);
+    });
+
+    it("displays the correct people name", () => {
+      cy.get(".MuiCardHeader-content").each(($card, index) => {
+        cy.wrap($card).find("p").contains(persons[index].name);
+      });
+    });
+  });
+
+  
+  describe("The people details page", () => {
+    before(() => {
+      cy.request(
+        `https://api.themoviedb.org/3/person/${
+          persons[0].id
+        }?api_key=${Cypress.env("TMDB_KEY")}`
+      )
+        .its("body")
+        .then((peopleDetails) => {
+          people = peopleDetails;
+        });
+    });
+    beforeEach(() => {
+      cy.visit(`/people/${persons[0].id}`);
+    });
+    it(" displays the people title, Biography  ", () => {
+      cy.get("h3").contains(people.name);
+      cy.get("h3").contains("Biography");
+     
+     
+         
+    });
+  });
+});
