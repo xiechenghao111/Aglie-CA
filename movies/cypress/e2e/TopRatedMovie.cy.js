ba3a95a57cb8df0d8165d5d41ba4a6d8ebe477b3
chenghao xie <1152759491@qq.com>
2022-11-20 16:06:37 +0000

    TopratedMovie

--- movies/cypress/e2e/TopRatedMovie.cy.js
+++ movies/cypress/e2e/TopRatedMovie.cy.js
@@ -1,6 +1,7 @@
 let movies; 
 let movie; 
-
+const filterByTitle = (movieList, string) =>
+  movieList.filter((m) => m.title.toLowerCase().search(string) !== -1);
 describe("TopRatedMovieBase test", () => {
   before(() => {
    
@@ -30,7 +31,35 @@ describe("TopRatedMovieBase test", () => {
       });
     });
   });
-
+  describe("Filtering", () => {
+    describe("By movie title", () => {
+     it("should only display movies with m in the title", () => {
+       let searchString = "m";
+       let matchingMovies = filterByTitle(movies, searchString);
+       cy.get("#filled-search").clear().type(searchString); // Enter m in text box
+       cy.get(".MuiCardHeader-content").should(
+         "have.length",
+         matchingMovies.length
+       );
+       cy.get(".MuiCardHeader-content").each(($card, index) => {
+         cy.wrap($card).find("p").contains(matchingMovies[index].title);
+       });
+     })
+     it("should only display movies with o in the title", () => {
+       let searchString = "o";
+       let matchingMovies = filterByTitle(movies, searchString);
+       cy.get("#filled-search").clear().type(searchString); // Enter m in text box
+       cy.get(".MuiCardHeader-content").should(
+         "have.length",
+         matchingMovies.length
+       );
+       cy.get(".MuiCardHeader-content").each(($card, index) => {
+         cy.wrap($card).find("p").contains(matchingMovies[index].title);
+       });
+     });
+    
+   });
+   
   
   describe("The movie details page", () => {
     before(() => {
@@ -62,4 +91,5 @@ describe("TopRatedMovieBase test", () => {
         });
     });
   });
+});
 });
