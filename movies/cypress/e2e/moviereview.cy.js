a61ced81b19a146c66ddd3822ddde848d37b56a4
chenghao xie <1152759491@qq.com>
2022-11-21 00:37:20 +0000

    movieReview

---
+++ movies/cypress/e2e/moviereview.cy.js
@@ -0,0 +1,91 @@
+let review;
+let movies; // List of movies from TMDB
+
+describe("Base tests", () => {
+  before(() => {
+    // Get the discover movies from TMDB and store them locally.
+    cy.request(
+      `https://api.themoviedb.org/3/discover/movie?api_key=${Cypress.env(
+        "TMDB_KEY"
+      )}&language=en-US&include_adult=false&include_video=false&page=1`
+    )
+      .its("body") // Take the body of HTTP response from TMDB
+      .then((response) => {
+        movies = response.results;
+      
+      });
+  });
+  beforeEach(() => {
+    cy.visit("/");
+  });
+
+  describe("The movie details page", () => {
+    before(() => {
+      cy.request(
+        `https://api.themoviedb.org/3/movie/${
+          movies[0].id
+        }?api_key=${Cypress.env("TMDB_KEY")}`
+      )
+        .its("body")
+        .then((movieDetails) => {
+          movie = movieDetails;
+        });
+    });
+  });
+describe("moviereview tests", () => {
+  before(() => {
+   
+    cy.request(
+      `https://api.themoviedb.org/3/movie/${ movies[0].id}/reviews?api_key=${Cypress.env("TMDB_KEY")}`
+    )
+      .its("body") // Take the body of HTTP response from TMDB
+      .then((response) => {
+        movies = response.results;
+      });
+  });
+  beforeEach(() => {
+    cy.visit(`/movies/436270`);
+  });
+
+  describe("The Review ", () => {
+    it("displays review button", () => {
+     
+      cy.get(".MuiGrid-grid-xs-9>button").contains("Reviews");
+     
+    });
+
+    
+
+    
+    
+  });
+  describe("moviereview content tests", () => {
+    before(() => {
+     
+      cy.request(
+        `https://api.themoviedb.org/3/movie/${ movies[0].id}/reviews?api_key=${Cypress.env("TMDB_KEY")}`
+      )
+        .its("body") // Take the body of HTTP response from TMDB
+        .then((response) => {
+          review = response.results;
+        });
+    });
+    beforeEach(() => {
+      cy.visit(`/movies/436270`);
+    });
+  it("displays review page", () => {
+
+   cy.get(".MuiGrid-grid-xs-9>button").click();
+
+    cy.get(".MuiTableHead-root tr th").eq(0).contains("Author");
+    cy.get(".MuiTableHead-root tr th").eq(1).contains("Excerpt");
+    cy.get(".MuiTableHead-root tr th").eq(2).contains("More");
+    
+    cy.get(".MuiTableRow-root  td").eq(1).contains("Full Review");
+   
+  });
+});
+});
+
+
+});
